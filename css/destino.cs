4a7b1b9ad749cea2c55aa084ed14fd62fb973c46
Federico De Meco <federico.demeco@hotmail.com>
2022-09-05 18:11:35 -0300

    subida inicial con estilos de hipervinculos del footer modificados

---
+++ css/destino.cs
@@ -0,0 +1,67 @@
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400;700&display=swap");
+h1 h2 {
+  color: red;
+  text-shadow: 3px 3px 3px black;
+}
+
+h3 h4 {
+  color: black;
+}
+
+h5 {
+  color: white;
+  text-shadow: 2px 2px 2px black;
+}
+
+/* Reseteo */
+* {
+  font-family: "Source Sans Pro", sans-serif;
+  margin: 0%;
+  padding: 0;
+}
+
+/** Variables **/
+@media (min-width: 360px) and (max-width: 599px) {
+  h1 {
+    font-size: 22px;
+  }
+  h2 {
+    font-size: 20px;
+  }
+  h3 {
+    font-size: 14px;
+  }
+  h4 {
+    font-size: 12px;
+  }
+  h5 {
+    font-size: 8px;
+  }
+}
+@media (min-width: 600px) and (max-width: 1199px) {
+  h1 {
+    font-size: 22px;
+  }
+  h2 {
+    font-size: 20px;
+  }
+  h3 {
+    font-size: 18px;
+  }
+  h4 {
+    font-size: 14px;
+  }
+  h5 {
+    font-size: 10px;
+  }
+}
+@media (min-width: 1200px) {
+  h3 {
+    font-size: 18px;
+  }
+  h4 {
+    font-size: 16px;
+  }
+}
+
+/*# sourceMappingURL=destino.cs.map */
